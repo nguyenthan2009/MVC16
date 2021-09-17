f2c84ac916a53082a8999c5d19aa78c33793b7cf
unknown <sauvole75@gmail.com>
2021-09-17 10:53:43 +0700

    add

---
+++ src/main/java/com/than/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.than.repository;
+
+import com.than.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/than/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.than.service;
+
+import com.than.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+
+}
